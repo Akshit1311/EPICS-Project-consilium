6ef7e8e286412451602a62115f6ad22bd9a8eccf
Akshit1311 <a4748g@gmail.com>
2022-02-25 15:44:47 +0530

    add: titles to pages

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import React from "react";
 
 const PageHeader: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div className="letter font-raleway text-7xl font-black tracking-[-.1em]">
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       {title}
       <div className="h-2 w-full bg-gradient-to-r from-[white] via-[#D7B605] to-[#D7B605]"></div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 // components
-import Navbar from '@components/Navbar'
-import Banner from '@components/Home/Banner'
-import SayHi from '@components/Home/SayHi/SayHi'
+import Navbar from "@components/Navbar";
+import Banner from "@components/Home/Banner";
+import SayHi from "@components/Home/SayHi/SayHi";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-[#12181B]">
       <Head>
-        <title>Create Next App</title>
+        <title>Consillium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
 
       <footer></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
